60de45104489e3b92b806c08b40adf8cb6422689
Samiksha-ag <samikshaagrawal7187@gmail.com>
2026-09-05 19:52:22 +0530

    Maximum Subarray - 2026-09-05T14:22:21.684Z

---
+++ 53-maximum-subarray/solution.cpp
@@ -0,0 +1,19 @@
+// 0 ms | 71.8 MB
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int prefix=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            prefix+=nums[i];
+            maxi=max(maxi,prefix);
+
+            if(prefix<0)
+            prefix=0;
+        }
+
+        return maxi;
+    }
+};
