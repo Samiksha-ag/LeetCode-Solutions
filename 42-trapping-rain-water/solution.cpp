db1d7aca51b8362ff788f2c98130cf1c6772d335
Samiksha-ag <samikshaagrawal7187@gmail.com>
2026-09-06 15:29:18 +0530

    Trapping Rain Water - 2026-09-06T09:59:18.445Z

---
+++ 42-trapping-rain-water/solution.cpp
@@ -0,0 +1,41 @@
+// 0 ms | 25.8 MB
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size(),water=0;
+        int leftmax=0,rightmax=0,maxheight=height[0],index=0;
+
+        // find highest building
+        for(int i=1;i<n;i++)
+        {
+            if(height[i]>maxheight)
+            {
+                maxheight=height[i];
+                index=i;
+            }
+        }
+
+        // left part
+
+        for(int i=0;i<index;i++)
+        {
+            if(leftmax>height[i])
+            water+=leftmax-height[i];
+            else
+            leftmax=height[i];
+        }
+
+        // right part
+        for(int i=n-1;i>index;i--)
+        {
+            if(rightmax>height[i])
+            water+=rightmax-height[i];
+            else
+            rightmax=height[i];
+        }
+
+        return water;
+
+
+    }
+};
