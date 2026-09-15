97d4245d07adc5f203083c1ce301bcfce42443e6
Samiksha Agrawal <samikshaagrawal7187@gmail.com>
2026-09-15 15:33:10 +0530

    First Unique Character in a String - 2026-09-15T10:03:09.096Z

---
+++ 387-first-unique-character-in-a-string/solution.cpp
@@ -0,0 +1,21 @@
+// 3 ms | 14.7 MB
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>ans(26,0);
+
+        for(int i=0;i<s.size();i++)
+        {
+            ans[s[i]-'a']++;
+        }
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(ans[s[i]-'a']==1)
+            return i;
+        }
+
+        return -1;
+
+    }
+};
