7a09c11621bc44b2608e0519753cce24c5f2555f
Samiksha Agrawal <samikshaagrawal7187@gmail.com>
2026-09-15 15:19:17 +0530

    Sort Vowels in a String - 2026-09-15T09:49:16.357Z

---
+++ 2785-sort-vowels-in-a-string/solution.cpp
@@ -0,0 +1,60 @@
+// 15 ms | 16.4 MB
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            {
+                lower[s[i]-'a']++;
+                s[i]='#';
+            }
+            else if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            {
+                upper[s[i]-'A']++;
+                s[i]='#';
+            }
+        }
+        string vowel;
+
+        for(int i=0;i<26;i++)
+        {
+            char c='A'+i;
+            while(upper[i])
+            {
+                vowel+=c;
+                upper[i]--;
+            }
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            char c='a'+i;
+            while(lower[i])
+            {
+                vowel+=c;
+                lower[i]--;
+            }
+        }
+        
+        // first point s[0] and second points vowel[0]
+        int first=0,second=0;
+
+        while(second<vowel.size())
+        {
+            if(s[first]=='#')
+            {
+                s[first]=vowel[second];
+                second++;
+            }
+
+            first++;
+        }
+
+        return s;
+
+    }
+};
