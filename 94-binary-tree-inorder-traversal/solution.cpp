a85f0fb082827ae5b79283d96e5fd29649d7961c
Samiksha Agrawal <samikshaagrawal7187@gmail.com>
2026-09-20 12:41:30 +0530

    Binary Tree Inorder Traversal - 2026-09-20T07:11:29.719Z

---
+++ 94-binary-tree-inorder-traversal/solution.cpp
@@ -0,0 +1,32 @@
+// 0 ms | 10.8 MB
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void Inorder(TreeNode* root, vector<int>&ans)
+    {
+        if(root==NULL)
+        return;
+
+        // Left
+        Inorder(root->left,ans);
+        // Node
+        ans.push_back(root->val);
+        // Right
+         Inorder(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        Inorder(root,ans);
+        return ans;
+    }
+};
